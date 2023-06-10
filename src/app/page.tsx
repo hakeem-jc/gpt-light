80c572e92c3c41238616f7e2d253b568e4d10ded
hakeem-jc <hakeem_clarke14@yahoo.com>
2023-06-10 12:50:20 +0200

    Setup layout with flexbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.app}>
+      <header className="app__header">
+        <h1 className='app__title'>BackChat</h1>
+      </header>
+
       
+      
+
     </main>
   )
 }
