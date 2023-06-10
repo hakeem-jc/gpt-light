33023efc35fa3a99c8ca41ca62b7591e0c0360b4
hakeem-jc <hakeem_clarke14@yahoo.com>
2023-06-10 18:50:04 +0200

    Add form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
 import styles from './page.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
   return (
     <main className={styles.app}>
-      <header className="app__header">
-        <h1 className='app__title'>BackChat</h1>
+      <header>
+        <h1 className={styles.app__title}>ChatGPT Light</h1>
+        <p>A minimalist ChatGPT Clone</p>
       </header>
 
-      
-      
+      <section className={styles.messages}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, deserunt?</p>
+      </section>
 
+      <section className={styles.chat_form_container}>
+        <form className={styles.chat_form}>
+            <input className={styles.chat_input} placeholder='Send a message.'/>
+            <FontAwesomeIcon icon={faPaperPlane} className={styles.icon}/>
+        </form>
+      </section>
     </main>
   )
 }
