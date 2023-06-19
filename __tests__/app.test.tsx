48071b0fc1f7fea28023e9a919186cdbd440448e
hakeem-jc <hakeem_clarke14@yahoo.com>
2023-06-19 11:49:53 +0200

    Add unit tests for smoke testing

---
+++ __tests__/app.test.tsx
@@ -0,0 +1,41 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from '@/redux/store';
+import Page from '@/app/page';
+import { setChat } from '@/redux/chatSlice';
+
+
+describe('Page', () => {
+  test('renders without errors', () => {
+    Element.prototype.scrollIntoView = jest.fn();
+
+    render(
+      <Provider store={store}>
+        <Page />
+      </Provider>
+    );
+
+  });
+
+  test('scrolls to the bottom when chat updates', () => {
+    const mockChat = [
+      { role: 'user', content: 'Hello' },
+      { role: 'assistant', content: 'Hi there!' },
+    ];
+
+    Element.prototype.scrollIntoView = jest.fn();
+
+    const mockDispatch = jest.fn();
+    jest.spyOn(store, 'dispatch').mockImplementation(mockDispatch);
+
+    render(
+      <Provider store={store}>
+        <Page />
+      </Provider>
+    );
+
+    store.dispatch(setChat({ role: 'user', content: 'Hello' }));
+    expect(Element.prototype.scrollIntoView).toHaveBeenCalled();
+    expect(mockDispatch).toHaveBeenCalledWith(setChat({ role: 'user', content: 'Hello' }));
+  });
+});
