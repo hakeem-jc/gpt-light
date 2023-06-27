a1b1dce0374fe5293f196d9b2ce379fb963274ef
hakeem-jc <hakeem_clarke14@yahoo.com>
2023-06-27 16:03:21 +0200

    Add initial prompt to shorten messages and enable multiple languages

--- src/redux/chatSlice.ts
+++ src/redux/chatSlice.ts
@@ -2,7 +2,9 @@ import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { Message } from '../interfaces/interfaces';
 
-const messages:Message[] = []
+const messages:Message[] = [
+    {"role": "system", "content": "You are ChatGPT, a large language model trained by OpenAI. Answer in the user's language based on the last message they sent. Answer as concisely as possible."}
+]
 
 const setChatSlice = createSlice({
     name: 'chat',
