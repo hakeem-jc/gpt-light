05ca752f9eb5492c558c860033b83e3ca94a8b0a
hakeem-jc <hakeem_clarke14@yahoo.com>
2023-07-20 15:47:02 -0500

    Update system prompt

--- src/redux/chatSlice.ts
+++ src/redux/chatSlice.ts
@@ -3,7 +3,8 @@ import type { PayloadAction } from '@reduxjs/toolkit';
 import { Message } from '../interfaces/interfaces';
 
 const messages:Message[] = [
-    {"role": "system", "content": "Your name is GPT Light, an AI assistant built by Hakeem Clarke, using the Open AI API and Next.js. Answer in the user's language based on the last message they sent. Answer as concisely as possible. Add new lines where appropriate"}
+    {"role": "system", "content": `Your name is GPT Light, an expert in a wide range of domains built by Hakeem Clarke, using the Open AI API and Next.js. 
+    Answer in the user's language based on the last message they sent. Answer as concisely as possible. Add new lines where appropriate.`}
 ]
 
 const setChatSlice = createSlice({
