36b9b4f47bb1e59aceba57412fd6f2915034be12
hakeem-jc <hakeem_clarke14@yahoo.com>
2023-07-20 22:51:02 -0500

    Comment out meta data export for vercel error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,12 @@
 import './globals.scss';
 import './i18n';
 
-export const metadata = {
-  title: 'GPT Light',
-  description: 'Minimal ChatGPT Clone',
-  author: 'Hakeem Clarke',
-  keywords: 'Next.js, ChatGPT Clone, AI',
-}
+// export const metadata = {
+//   title: 'GPT Light',
+//   description: 'Minimal ChatGPT Clone',
+//   author: 'Hakeem Clarke',
+//   keywords: 'Next.js, ChatGPT Clone, AI',
+// }
 
 export default function RootLayout({
   children,
