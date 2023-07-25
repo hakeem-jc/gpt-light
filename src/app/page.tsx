83dc9caf9ce4d065ca15fe948bcefd6356effdc5
hakeem-jc <hakeem_clarke14@yahoo.com>
2023-07-24 20:42:58 -0500

    Move metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,14 @@
 import { Provider } from 'react-redux';
 import { store } from '@/redux/store';
 import { App } from './app';
-import { SnackbarProvider, enqueueSnackbar } from 'notistack';
+import { SnackbarProvider } from 'notistack';
+
+export const metadata = {
+  title: 'GPT Light',
+  description: 'Minimal AI Chat bot',
+  author: 'Hakeem Clarke',
+  keywords: 'ChatGPT Alternative, AI, Chatbot',
+}
 
 export default function Page() {
   return (
