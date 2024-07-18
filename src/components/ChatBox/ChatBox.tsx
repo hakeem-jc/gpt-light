6eb8abd3456be3b8d4533e74e91b8b6ac0fa43dc
hakeem-jc <hakeemclarke14@gmail.com>
2024-07-17 23:16:32 -0500

    feat: Format response text

--- src/components/ChatBox/ChatBox.tsx
+++ src/components/ChatBox/ChatBox.tsx
@@ -1,47 +1,41 @@
-import { FC } from 'react';
-import styles from './ChatBox.module.scss';
-import user from '@/assets/user.png';
-import avatar from '@/assets/bot.png';
-import { ChatType } from '@/interfaces/interfaces';
-import Image from 'next/image';
+import { FC } from "react";
+import styles from "./ChatBox.module.scss";
+import user from "@/assets/user.png";
+import avatar from "@/assets/bot.png";
+import { ChatType } from "@/interfaces/interfaces";
+import Image from "next/image";
 
 interface ChatBoxProps {
-    role?: string,
-    content?: string,
-    is_empty?: boolean
+  role?: string;
+  content?: string;
+  is_empty?: boolean;
 }
 
-const ChatBox:FC<ChatBoxProps> = (props) => {
-    const roleClassName = props.role === ChatType.BOT ? styles['chatbox-bot'] : '';
-  
-    return (
-        <div className={`${styles.chatbox} ${roleClassName}`}>
-        {props.is_empty ? 
-            <div className={styles['text-container']}>
-                <Image 
-                    src={avatar}
-                    alt="Bot Icon"
-                    className={styles['image']}
-                />
-                <div className={styles.cursor}></div>
-            </div>
-            :
-            <div className={styles['text-container']}>
-                <Image 
-                    src={props.role === ChatType.BOT ? avatar : user}
-                    alt={props.role === ChatType.BOT ? "Bot Icon" : "User Icon"}
-                    className={styles['image']}
-                />
-                <div className={styles['text-wrap']}>
+const ChatBox: FC<ChatBoxProps> = (props) => {
+  const roleClassName =
+    props.role === ChatType.BOT ? styles["chatbox-bot"] : "";
 
-                <p className={styles['text']}>
-                    {props.content}
-                </p>
-                </div>
-            </div>
-        }
-
-      </div>);
-}
+  return (
+    <pre className={`${styles.chatbox} ${roleClassName}`}>
+      {props.is_empty ? (
+        <div className={styles["text-container"]}>
+          <Image src={avatar} alt="Bot Icon" className={styles["image"]} />
+          <div className={styles.cursor}></div>
+        </div>
+      ) : (
+        <div className={styles["text-container"]}>
+          <Image
+            src={props.role === ChatType.BOT ? avatar : user}
+            alt={props.role === ChatType.BOT ? "Bot Icon" : "User Icon"}
+            className={styles["image"]}
+          />
+          <div className={styles["text-wrap"]}>
+            <p className={styles["text"]}>{props.content}</p>
+          </div>
+        </div>
+      )}
+    </pre>
+  );
+};
 
 export default ChatBox;
