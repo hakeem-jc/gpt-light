c946bff462b4afa5a6ff89817d10041a5696f348
hakeem-jc <hakeemclarke14@gmail.com>
2024-07-17 22:19:54 -0500

    feat: Remove nextjs-cors and change package title

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -1,18 +1,11 @@
 import { OpenAI } from "openai";
 import { NextApiRequest, NextApiResponse } from "next";
-import NextCors from 'nextjs-cors';
 
 const openai = new OpenAI();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
   try {
-    await NextCors(req, res, {
-      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-      origin: process.env.NEXT_PUBLIC_API_URL,
-      optionsSuccessStatus: 200,
-    });
-
     const messages = req.body.messages || [];
     if (!Array.isArray(messages) || messages.length === 0) {
       res.status(400).json({
