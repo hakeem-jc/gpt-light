2b8f2f48f5c8dff4991544dc33e353f93a0e1537
Hakeem Clarke <hakeem.clarke@speurgroup.com>
2024-11-28 13:52:36 +0100

    feat: Update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Providers from './providers';
 
 export const metadata = {
-  title: 'GPT Light',
-  description: 'Minimal AI Chat bot',
+  title: 'GPT Light - Fast & Minimal AI Chatbot Alternative',
+  description: 'Discover GPT Light, a lightning-fast and lightweight AI chatbot alternative. Enjoy seamless conversations powered by AI with simplicity and efficiency.',
   author: 'Hakeem Clarke',
-  keywords: 'ChatGPT Alternative, AI, Chatbot',
-}
+  keywords: 'AI chatbot, ChatGPT alternative, minimal chatbot, lightweight AI, conversational AI, GPT Light, fast chatbot',
+};
 
 export default function Page() {
   return (
